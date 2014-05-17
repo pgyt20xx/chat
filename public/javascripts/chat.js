55e5fbf53e4db50c2e8f0c481946cbcfd5af70fc
yuichi282 <yuichi282@gmail.com>
2014-05-17 22:38:11 +0900

    fix

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,5 @@
 $(function() {
-  var socket = io.connect('http://localhost');
+  var socket = io.connect('http://chat282.herokuapp.com/');
   socket.on('connect', function() {
     console.log('connected');
   });
